2d837379634cdac2658a0b279dd472b2d4ea5d98
niuqinghua <niuqinghua@letv.com>
2015-10-23 15:41:39 +0800

    add hello world sample

---
+++ prehnite-samples/prehnite-helloworld/src/main/java/dream/prehnite/sample/HelloWorldSample.java
@@ -0,0 +1,12 @@
+package dream.prehnite.sample;
+
+/**
+ * Created by niuqinghua on 15/10/23.
+ */
+public class HelloWorldSample {
+
+    public static void main(String[] args) {
+        System.out.printf("hello world!");
+    }
+
+}
